7a70c4d66e413326e40d27e67b6f7c1e737d40f5
Leejaeyeop <dlwoduq78@naver.com>
2024-11-04 22:49:17 +0900

    <feat>: add flame.tsx for first section
    - add flame.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div></div>;
+import { WideFlame } from "./flame";
+
+export default function About() {
+  return (
+    <div className="text-white">
+      <h1 className="font-extrabold text-7xl">성장에 목마른 개발자 이재엽</h1>
+      <div className="mo-fire">
+        <WideFlame />
+      </div>
+    </div>
+  );
 }
