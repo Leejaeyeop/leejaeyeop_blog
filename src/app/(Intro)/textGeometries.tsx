e91eaedc514aa08e48542048259b11852931abf7
Leejaeyeop <dlwoduq78@naver.com>
2024-11-05 22:45:09 +0900

    <fix>:  add declare module lines for TextGeometry typescript
    - add declare module lines for TextGeometry  to ressolve typescript error

--- src/app/(Intro)/textGeometries.tsx
+++ src/app/(Intro)/textGeometries.tsx
@@ -1,11 +1,18 @@
 import { motion } from "framer-motion-3d";
-import { extend, useThree } from "@react-three/fiber";
+import { extend, Object3DNode, useThree } from "@react-three/fiber";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
 import myFont from "../Roboto_Bold.json";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import { useEffect, useRef, useState } from "react";
 
 extend({ TextGeometry });
+
+// Add types to ThreeElements elements so primitives pick up on it
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    textGeometry: Object3DNode<TextGeometry, typeof TextGeometry>;
+  }
+}
 
 // @ts-ignore
 const font = new FontLoader().parse(myFont);
@@ -65,18 +72,20 @@ const Growth = () => {
     -6.5, 10, 2.5,
   ]);
   const GowthRef = useRef(null);
+  const GowthRef2 = useRef(null);
 
   useEffect(() => {
     const meshChallenge = scene.getObjectByName("meshChallenge");
 
     const { x, z } = GowthRef.current.getWorldPosition(meshChallenge.position);
+    GowthRef.current.lookAt(meshChallenge.position);
     setPosition([x, position[1], z]);
   }, []);
 
   return (
     <group dispose={null}>
-      <motion.group ref={GowthRef} position={position}>
-        <motion.mesh receiveShadow castShadow>
+      <motion.group ref={GowthRef} position={[-6.5, 10, 2.5]}>
+        <motion.mesh ref={GowthRef2} receiveShadow castShadow>
           <textGeometry
             args={["G", { font: font, size: 3, height: 1 }]}
           ></textGeometry>
