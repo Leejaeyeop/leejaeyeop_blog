99fbd0baf3d20c724057d2de69e7a968fffd92d1
이재엽 <leejaeyeop@onycom.com>
2024-11-07 16:31:30 +0900

    <feat>: add experience text to section
    -  add experience text to section

--- src/app/about/(experienceSection)/section.tsx
+++ src/app/about/(experienceSection)/section.tsx
@@ -1,5 +1,9 @@
 const ExperienceSection = () => {
-  return <section></section>;
+  return (
+    <section className=" text-black m-40 text-2xl">
+      <h1 className=" font-extrabold text-9xl">Experience</h1>
+    </section>
+  );
 };
 
 export default ExperienceSection;
