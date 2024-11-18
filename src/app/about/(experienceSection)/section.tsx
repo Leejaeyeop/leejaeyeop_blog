63f5016723b915efc6925a5de35ca44494ca4ef5
이재엽 <leejaeyeop@onycom.com>
2024-11-18 10:21:03 +0900

    <refactor>: refactor experience section
    - experience section

--- src/app/about/(experienceSection)/section.tsx
+++ src/app/about/(experienceSection)/section.tsx
@@ -12,60 +12,60 @@ import {
 } from "./articles";
 import { Courgette } from "next/font/google";
 import { throttle } from "lodash";
+import { Separator } from "@/components/atom/separator";
+import { Props } from "./articles";
+
 const courgette = Courgette({
   weight: ["400"],
   subsets: ["latin"],
 });
-import { Separator } from "@/components/atom/separator";
-import { Props } from "./articles";
 
-const CommonSection = ({
-  articles,
-  imageSrcs,
-}: {
+type CommonSectionProps = {
   articles: React.ComponentType<Props>[];
   imageSrcs: string[];
-}) => {
-  const [curSection, updateCursection] = useState(-1);
-  // useMemo를 사용해 articles.length에 따라 참조 배열 생성
+};
+
+const CommonSection = ({ articles, imageSrcs }: CommonSectionProps) => {
+  const [currentSection, setCurrentSection] = useState(-1);
+
+  // Memoize section references based on the number of articles
   const sectionRefs = useMemo(
-    () =>
-      Array.from({ length: articles.length }, () =>
-        createRef<HTMLDivElement>()
-      ),
-    [articles.length]
+    () => articles.map(() => createRef<HTMLDivElement>()),
+    [articles]
   );
 
+  // Smooth scrolling to the current section
   useEffect(() => {
-    if (curSection >= 0) {
-      // 해당 인덱스의 섹션으로 부드럽게 스크롤
-      sectionRefs[curSection].current.scrollIntoView({ behavior: "smooth" });
+    if (currentSection >= 0) {
+      sectionRefs[currentSection]?.current?.scrollIntoView({
+        behavior: "smooth",
+      });
     }
-  }, [curSection, sectionRefs]);
+  }, [currentSection, sectionRefs]);
 
-  const setCursection = useCallback(
-    throttle(
-      idx => {
-        updateCursection(idx);
-      },
-      300,
-      { leading: true, trailing: false }
-    ),
+  // Throttled section update
+  const updateCurrentSection = useCallback(
+    throttle((index: number) => setCurrentSection(index), 300, {
+      leading: true,
+      trailing: false,
+    }),
     []
   );
 
   return (
     <div className="flex gap-96">
-      {/* image set */}
+      {/* Draggable Image Set */}
       <DraggableContainer
-        curSection={curSection}
-        setCursection={setCursection}
+        curSection={currentSection}
+        setCursection={updateCurrentSection}
         srcs={imageSrcs}
       />
+
+      {/* Articles */}
       <div className="flex flex-col w-full sm:w-[calc(100%-300px)]">
-        {articles.map((Article, idx) => (
-          <div ref={sectionRefs[idx]} key={idx}>
-            <Article idx={idx} setCursection={setCursection}></Article>
+        {articles.map((Article, index) => (
+          <div ref={sectionRefs[index]} key={index}>
+            <Article idx={index} setCursection={updateCurrentSection} />
           </div>
         ))}
       </div>
@@ -73,55 +73,48 @@ const CommonSection = ({
   );
 };
 
-const WorkSection = () => {
-  return (
-    <CommonSection
-      articles={[ImqaArticle, DalgonaArticle, AsianArticle, BareunArticle]}
-      imageSrcs={["imqa.webp", "dalgona.webp", "asian.webp", "bareun.webp"]}
-    ></CommonSection>
-  );
-};
-
-const PersonalSection = () => {
-  return (
-    <CommonSection
-      articles={[FairyTaleArticle, ReactProgressBarArticle]}
-      imageSrcs={["fairyTale.webp", "reactProgressBar.webp"]}
-    ></CommonSection>
-  );
-};
-
-const ExperienceSection = () => {
-  return (
-    <section
-      id="experience"
-      className="text-2xl 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center"
+const SectionHeader = ({ title }: { title: string }) => (
+  <>
+    <h2
+      className={`${courgette.className} font-extrabold text-6xl break-words`}
     >
-      <div className="2xl:w-[1536px] w-full">
-        <h1
-          className={
-            courgette.className + " font-extrabold text-9xl mb-16 break-words"
-          }
-        >
-          Experience
-        </h1>
-        <h2 className={courgette.className + " font-extrabold text-6xl"}>
-          Work
-        </h2>
-        <Separator className="mb-20"></Separator>
-        <WorkSection></WorkSection>
-        <h2
-          className={
-            courgette.className + " font-extrabold text-6xl break-words"
-          }
-        >
-          Personal
-        </h2>
-        <Separator className="mb-20"></Separator>
-        <PersonalSection></PersonalSection>
-      </div>
-    </section>
-  );
-};
+      {title}
+    </h2>
+    <Separator className="mb-20" />
+  </>
+);
+
+const WorkSection = () => (
+  <CommonSection
+    articles={[ImqaArticle, DalgonaArticle, AsianArticle, BareunArticle]}
+    imageSrcs={["imqa.webp", "dalgona.webp", "asian.webp", "bareun.webp"]}
+  />
+);
+
+const PersonalSection = () => (
+  <CommonSection
+    articles={[FairyTaleArticle, ReactProgressBarArticle]}
+    imageSrcs={["fairyTale.webp", "reactProgressBar.webp"]}
+  />
+);
+
+const ExperienceSection = () => (
+  <section
+    id="experience"
+    className="text-2xl 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center"
+  >
+    <div className="2xl:w-[1536px] w-full">
+      <h1
+        className={`${courgette.className} font-extrabold text-9xl mb-16 break-words`}
+      >
+        Experience
+      </h1>
+      <SectionHeader title="Work" />
+      <WorkSection />
+      <SectionHeader title="Personal" />
+      <PersonalSection />
+    </div>
+  </section>
+);
 
 export default ExperienceSection;
