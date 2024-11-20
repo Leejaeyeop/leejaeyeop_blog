7ef3cf1e9820f5b74b70428eac5da3aec816ed58
이재엽 <leejaeyeop@onycom.com>
2024-11-20 09:41:48 +0900

    <reformat>: delete unused motion element
    - delete unused motion element

--- src/app/(Intro)/textGeometries.tsx
+++ src/app/(Intro)/textGeometries.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion-3d";
 import { extend, Object3DNode, useFrame, useThree } from "@react-three/fiber";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
 import myFont from "../Roboto_Bold.json";
@@ -18,28 +17,25 @@ declare module "@react-three/fiber" {
 const font = new FontLoader().parse(myFont);
 const Impossible = () => {
   return (
-    <group dispose={null}>
-      <motion.mesh receiveShadow castShadow></motion.mesh>
-      <motion.group position={[-6.5, -0.5, 2.5]}>
-        <motion.mesh receiveShadow castShadow rotation={[0, 0, -0.1]}>
-          <textGeometry
-            args={["I", { font: font, size: 2.5, height: 1 }]}
-          ></textGeometry>
-          <meshStandardMaterial roughness={0.5} color="#ff0000" />
-        </motion.mesh>
-        <motion.mesh
-          receiveShadow
-          castShadow
-          rotation={[0, 0, -0.1]}
-          position={[1.2, 0, 0]}
-        >
-          <textGeometry
-            args={["mpossible", { font: font, size: 2, height: 1 }]}
-          ></textGeometry>
-          {/* 텍스트 색상 설정 */}
-          <meshStandardMaterial roughness={0.5} color="#ff0000" />{" "}
-        </motion.mesh>
-      </motion.group>
+    <group dispose={null} position={[-6.5, -0.5, 2.5]}>
+      <mesh receiveShadow castShadow rotation={[0, 0, -0.1]}>
+        <textGeometry
+          args={["I", { font: font, size: 2.5, height: 1 }]}
+        ></textGeometry>
+        <meshStandardMaterial roughness={0.5} color="#ff0000" />
+      </mesh>
+      <mesh
+        receiveShadow
+        castShadow
+        rotation={[0, 0, -0.1]}
+        position={[1.2, 0, 0]}
+      >
+        <textGeometry
+          args={["mpossible", { font: font, size: 2, height: 1 }]}
+        ></textGeometry>
+        {/* 텍스트 색상 설정 */}
+        <meshStandardMaterial roughness={0.5} color="#ff0000" />{" "}
+      </mesh>
     </group>
   );
 };
@@ -54,22 +50,20 @@ const Challenge = ({ rigidChallenge }) => {
     );
   });
   return (
-    <group dispose={null}>
-      <motion.group position={[-6.5, 10, 2.5]} name="meshChallenge">
-        <motion.mesh receiveShadow castShadow>
-          <textGeometry
-            args={["C", { font: font, size: 3, height: 1 }]}
-          ></textGeometry>
-          <meshStandardMaterial roughness={0.5} color="#0099FF" />
-        </motion.mesh>
-        <motion.mesh receiveShadow castShadow position={[2.7, 0, 0]}>
-          <textGeometry
-            args={["hallenge", { font: font, size: 2, height: 1 }]}
-          ></textGeometry>
-          <meshStandardMaterial roughness={0.5} color="#0099FF" />
-          {/* 텍스트 색상 설정 */}
-        </motion.mesh>
-      </motion.group>
+    <group dispose={null} position={[-6.5, 10, 2.5]} name="meshChallenge">
+      <mesh receiveShadow castShadow>
+        <textGeometry
+          args={["C", { font: font, size: 3, height: 1 }]}
+        ></textGeometry>
+        <meshStandardMaterial roughness={0.5} color="#0099FF" />
+      </mesh>
+      <mesh receiveShadow castShadow position={[2.7, 0, 0]}>
+        <textGeometry
+          args={["hallenge", { font: font, size: 2, height: 1 }]}
+        ></textGeometry>
+        <meshStandardMaterial roughness={0.5} color="#0099FF" />
+        {/* 텍스트 색상 설정 */}
+      </mesh>
     </group>
   );
 };
@@ -89,22 +83,20 @@ const Growth = () => {
   }, []);
 
   return (
-    <group dispose={null}>
-      <motion.group ref={GowthRef} position={position}>
-        <motion.mesh receiveShadow castShadow>
-          <textGeometry
-            args={["G", { font: font, size: 3, height: 1 }]}
-          ></textGeometry>
-          <meshStandardMaterial roughness={0.5} color="#00ff95" />
-        </motion.mesh>
-        <motion.mesh receiveShadow castShadow position={[2.7, 0, 0]}>
-          <textGeometry
-            args={["Rowth", { font: font, size: 2, height: 1 }]}
-          ></textGeometry>
-          <meshStandardMaterial roughness={0.5} color="#00ff95" />
-          {/* 텍스트 색상 설정 */}
-        </motion.mesh>
-      </motion.group>
+    <group dispose={null} ref={GowthRef} position={position}>
+      <mesh receiveShadow castShadow>
+        <textGeometry
+          args={["G", { font: font, size: 3, height: 1 }]}
+        ></textGeometry>
+        <meshStandardMaterial roughness={0.5} color="#00ff95" />
+      </mesh>
+      <mesh receiveShadow castShadow position={[2.7, 0, 0]}>
+        <textGeometry
+          args={["Rowth", { font: font, size: 2, height: 1 }]}
+        ></textGeometry>
+        <meshStandardMaterial roughness={0.5} color="#00ff95" />
+        {/* 텍스트 색상 설정 */}
+      </mesh>
     </group>
   );
 };
@@ -114,16 +106,14 @@ const Leejaeyeop = () => {
   camera.position.set(-3, 4, 10);
 
   return (
-    <group scale={[1.3, 1.3, 1.3]} dispose={null}>
-      <motion.group position={[-11, 20, 10]}>
-        <motion.mesh receiveShadow castShadow position={[2.7, 0, 0]}>
-          <textGeometry
-            args={["Jaeyeop", { font: font, size: 2, height: 1 }]}
-          ></textGeometry>
-          <meshStandardMaterial roughness={0.5} color="#00ff95" />
-          {/* 텍스트 색상 설정 */}
-        </motion.mesh>
-      </motion.group>
+    <group scale={[1.3, 1.3, 1.3]} position={[-10, 20, 12]} dispose={null}>
+      <mesh receiveShadow castShadow>
+        <textGeometry
+          args={["Jaeyeop", { font: font, size: 2, height: 1 }]}
+        ></textGeometry>
+        <meshStandardMaterial roughness={0.5} color="#00ff95" />
+        {/* 텍스트 색상 설정 */}
+      </mesh>
     </group>
   );
 };
