2c5076a74eba9e089f67cc1483cda7f118f66e62
Leejaeyeop <dlwoduq78@naver.com>
2025-04-15 15:47:22 +0900

    <refactor>: refactor sequence gererator
    - refactor sequence gererator

---
+++ src/features/scene/intro/hooks/sequence/sequenceInitial.ts
@@ -0,0 +1,27 @@
+export type SequenceInfo = typeof sequenceInitial;
+
+// default
+export const sequenceInitial = {
+  // 배경색
+  backgroundColor: "#FFA500",
+  // Text color
+  color: "#FF6D00",
+  // Text
+  text: "불가능",
+  // TextGeometries
+  showImpossible: true,
+  showChallenge: false,
+  showGrowth: false,
+  showLeejaeyeop: false,
+  showCuboidCollider: true,
+  // Camera Shake
+  shakeX: false,
+  shakeY: false,
+  // time to delay for next sequence
+  delayTime: 0,
+  h1Animation: null,
+  showH2Text: false,
+  changeLightPos: false,
+  isDone: false,
+  bgGradient: "bg-gradient-to-br",
+};
