0f58ee4156c9e5a33602e11ca833dc5119f90261
Leejaeyeop <dlwoduq78@naver.com>
2025-05-16 22:44:09 +0900

    <feat>: decrease noise intense

--- src/shaders/theater.frag.glsl
+++ src/shaders/theater.frag.glsl
@@ -2,10 +2,10 @@ varying vec2 vUv;
 uniform float time;
 
 float random(vec2 st) {
-  return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+  return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
 }
 
 void main() {
   float noise = random(vUv + time);
-  gl_FragColor = vec4(vec3(noise * 0.5), 1.0);
-} 
+  gl_FragColor = vec4(vec3(noise * 0.15), 1.0); // 0.5 → 0.2로 감소시켜 어둡게 처리
+}
--- src/shaders/theater.vert.glsl
+++ src/shaders/theater.vert.glsl
@@ -2,7 +2,6 @@ varying vec2 vUv;
 uniform float time;
 
 void main() {
-  // 시간에 따라 vUv 좌표를 약간씩 움직여서 지지직거리는 효과를 줍니다.
   vec2 displacedUV = uv + vec2(sin(time * 10.0 + uv.y * 50.0) * 0.01, cos(time * 15.0 + uv.x * 50.0) * 0.01);
   vUv = displacedUV;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
